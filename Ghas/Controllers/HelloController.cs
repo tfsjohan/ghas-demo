2b29dc8766b825d07217afbeff098b8d2c6f4028
Johan Nordberg <johan@ifokus.se>
2024-03-08 22:41:43 +0100

    Resize with bad version of ImageSharp

--- Ghas/Controllers/HelloController.cs
+++ Ghas/Controllers/HelloController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
 
 namespace Ghas.Controllers;
 
@@ -11,5 +13,19 @@ public class HelloController(ILogger<HelloController> Logger) : ControllerBase
     {
         Logger.LogInformation("Saying hello to {name}", name);
         return $"Hello, {name}!";
+    }
+
+    [HttpGet("image")]
+    public IActionResult GetImage(int w = 100, int h = 100)
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "image.png");
+
+        // Resize image with ImageSharp
+        using var image = Image.Load(path);
+        image.Mutate(x => x.Resize(w, h));
+        using var memoryStream = new MemoryStream();
+        image.SaveAsPng(memoryStream);
+        memoryStream.Position = 0;
+        return File(memoryStream, "image/png");
     }
 }
